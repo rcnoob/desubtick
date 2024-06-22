a69e561d5ea56794ce9c5d8ea82d92932fd86eff
rcnoob <rcnoobclub@gmail.com>
2024-06-22 02:29:07 -0400

    update
    - add defaults to configure.py
    - add mms submodule

--- configure.py
+++ configure.py
@@ -19,13 +19,13 @@ if ambuild_version.startswith('2.1'):
   sys.exit(1)
 
 parser = run.BuildParser(sourcePath=sys.path[0], api='2.2')
-parser.options.add_argument('-n', '--plugin-name', type=str, dest='plugin_name', default=None,
+parser.options.add_argument('-n', '--plugin-name', type=str, dest='plugin_name', default='desubtick',
                        help='Plugin name')
-parser.options.add_argument('-a', '--plugin-alias', type=str, dest='plugin_alias', default=None,
+parser.options.add_argument('-a', '--plugin-alias', type=str, dest='plugin_alias', default='desubtick',
                        help='Plugin alias')
-parser.options.add_argument('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
+parser.options.add_argument('--hl2sdk-root', type=str, dest='hl2sdk_root', default='../',
                        help='Root search folder for HL2SDKs')
-parser.options.add_argument('--mms_path', type=str, dest='mms_path', default=None,
+parser.options.add_argument('--mms_path', type=str, dest='mms_path', default='../metamod-source',
                        help='Metamod:Source source tree folder')
 parser.options.add_argument('--enable-debug', action='store_const', const='1', dest='debug',
                        help='Enable debugging symbols')
